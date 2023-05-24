bedf325e7be59caebf3dd63d3ea87809c309eefb
rc2pc2 <petriccarcc@gmail.com>
2023-05-24 10:47:31 +0200

    added up choice and simple functions

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,54 @@
 const playButton = document.querySelector('header button.button-play');
-
-const gridElement = document.querySelector('div.grid');
-
 playButton.addEventListener(('click'), function(){
-    gridElement.innerHTML = "";
+    startNewGame();
+});
 
-    for (let index = 0; index < 100; index++) {
-        const newCell = document.createElement('div');
-        newCell.classList.add('cell');
-        newCell.innerHTML = `
-            <p>
-                ${index + 1}
-            </p>`;
+function startNewGame(){
+    const gridElement = document.querySelector('div.grid');
+
+    // value di una select
+    const level = parseInt(document.getElementById('level-select').value);
+
+    let cellsNumber = 0;
+    let cellsClass = "";
+
+    if (level === 0){
+        cellsNumber = 100;
+        cellsClass = 'cell-easy';
+    } else if (level === 1){
+        cellsNumber = 81;
+        cellsClass = 'cell-medium';
+    } else {
+        cellsNumber = 49;
+        cellsClass = 'cell-hard';
+    }
+
+    gridElement.innerHTML = "";
+    console.clear();
+
+    for (let index = 0; index < cellsNumber; index++) {
+        const newCell = createElement('div','cell '+ cellsClass,
+                        `<p>${index + 1}</p>`);
 
         newCell.addEventListener('click', function(){
             console.log(index + 1);
-            newCell.classList.toggle('active');
+            this.classList.toggle('active'); // this === newCell
         });
 
         gridElement.appendChild(newCell);
     }
-});
+}
 
+/**
+ * Function that creates a custom HTML element with the given tag and classes (as a string)
+ *
+ * @param {string} tagName The tag of the element to be created as a string
+ * @param {string} className The classes of the element to be created as a string
+ * @param {string} htmlContent The content of the element to be created as a string, including html tags.
+ */
+function createElement(tagName, className, htmlContent){
+    const htmlElement = document.createElement(tagName);
+    htmlElement.className = className;
+    htmlElement.innerHTML = htmlContent;
+    return htmlElement;
+}
