81105a96ed1048b131ed252eb026fd562dda0337
rc2pc2 <petriccarcc@gmail.com>
2023-05-24 12:00:36 +0200

    added function getRandomUniqueNumber()

--- js/myscript.js
+++ js/myscript.js
@@ -49,3 +49,41 @@ function createElement(tagName, className, htmlContent){
     htmlElement.innerHTML = htmlContent;
     return htmlElement;
 }
+
+/**
+ * Function that generates an array of random unique numbers between two values (both included).
+ *
+ * @param minNum The minimum interval for the random generated numbers
+ * @param maxNum The maximum interval for the random generated numbers
+ * @param elements The number of elements to be generated
+ * @returns The list of random unique generated numbers, or an empty array if it is not possibile to generate that amount of numbers within the given interval.
+ */
+function getRandomUniqueNumber( minNum, maxNum, elements ){
+    const numbersList = [];
+
+    if ( (maxNum - minNum) < elements ){
+        return [];
+    }
+
+    while (numbersList.length < elements){
+        const newRandomNumber = getRandomInt(minNum, maxNum);
+        if (!numbersList.includes(newRandomNumber)){
+            numbersList.push(newRandomNumber);
+        }
+    }
+
+    return numbersList;
+}
+
+/**
+ * Function that generates a random number (not secure) between two values, both included.
+ *
+ * @param minumNumber the included minium value of the random generated number range.
+ * @param maximumNumber the included maximum value of the random generated number range
+ * @returns A randomly generated number.
+ */
+function getRandomInt(minumNumber, maximumNumber){
+    const randomNumber = Math.floor( Math.random() * ( maximumNumber - minumNumber +1) + minumNumber);
+
+    return randomNumber;
+}
