61afe5c1c84ec422ceb880dc471f65e0b1fab5f3
rc2pc2 <petriccarcc@gmail.com>
2023-05-25 10:39:51 +0200

    first simple implementation of a mine sweeper game

--- js/myscript.js
+++ js/myscript.js
@@ -6,9 +6,12 @@ playButton.addEventListener(('click'), function(){
 function startNewGame(){
     const gridElement = document.querySelector('div.grid');
     const level = parseInt(document.getElementById('level-select').value);
+    const outputBanner = document.querySelector('h2.info-banner');
 
+    let userScore = 0;
     let cellsNumber = 0;
     let cellsClass;
+    let isGameOver = false;
 
     if (level === 0){
         cellsNumber = 100;
@@ -21,16 +24,45 @@ function startNewGame(){
         cellsClass = 'cell-hard';
     }
 
+    const bombsList = getRandomUniqueNumber(1, cellsNumber, 16);
+    console.log(bombsList);
+
+    outputBanner.innerHTML = "Welcome! Click a cell to get a point!";
     gridElement.innerHTML = "";
+    gridElement.classList.remove('game-over');
 
     for (let index = 0; index < cellsNumber; index++) {
         const newCell = createElement('div','cell '+ cellsClass,
-                        `<p>${index + 1}</p>`);
+        ``);
+        const isThisABomb = bombsList.includes(index + 1);
+
+        if (isThisABomb){
+            newCell.classList.add('bomb');
+            newCell.innerHTML = '<p><i class="fa-solid fa-bomb"></i></p>';
+        }
 
         newCell.addEventListener('click', function(){
-            console.log(index + 1);
-            this.classList.toggle('active'); // this === newCell
-        });
+            if (!isGameOver){
+                if ( isThisABomb ){ // user clicked a bomb
+                    outputBanner.innerHTML = "Game over, your score is: "+ userScore;
+                    gridElement.classList.add('game-over');
+                    isGameOver = true;
+                } else { // not a bomb
+                    userScore++;
+                    outputBanner.innerHTML = "You scored a point! Your score is: "+ userScore;
+                    console.log(index + 1, userScore);
+                    this.classList.add('active'); // this === newCell
+                    this.innerHTML = '<img src="./img/flower.png" alt="A tiny little flower">';
+
+                    if ( userScore === cellsNumber - bombsList.length){
+                        outputBanner.innerHTML = "WOW!! YOU WON!! HIGHSCORE: "+ userScore;
+                        isGameOver = true;
+                    }
+                }
+            } else {
+                alert('La partita è finita, per continuare inizia una nuova partita');
+            }
+        }, {once: true});
 
         gridElement.appendChild(newCell);
     }
